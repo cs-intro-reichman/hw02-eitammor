282372a9b38a6480e0f7d8baa0dc7a0e85e15f86
eitammor <154350216+eitammor@users.noreply.github.com>
2024-01-02 14:25:43 +0200

    Update DamkaBoard.java

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -3,10 +3,11 @@ public class DamkaBoard {
 		int num = Integer.parseInt ( args[0] );
 		for(int i = 0; i<num; i++)
 		{
-			for (int j = 0; j<num; j++)
+			for (int j = 0; j<num-1; j++)
 			{
 				System.out.print("* ");
 			}
+			System.out.print("*");
 			System.out.println();
 		}
 	}
