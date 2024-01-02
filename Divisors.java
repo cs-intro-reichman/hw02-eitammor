5f2d80fc77850f7b0b8ca2b86e302f1a6063eb11
eitammor <154350216+eitammor@users.noreply.github.com>
2024-01-02 14:18:52 +0200

    HW2

--- Divisors.java
+++ Divisors.java
@@ -1,8 +1,23 @@
-/**
- *  Gets a command-line argument (int), and prints all the divisors of the given number.
- */
 public class Divisors {
-	public static void main (String[] args) {
-		//// Put your code here
+	public static void main(String[] args) {
+		int num = Integer.parseInt ( args[0] );
+		int div = 1;
+		if (num!=0)
+		{
+			while (div < num)
+			{
+				if ((num%div)==0)
+				{
+					System.out.println(div);
+				}
+				div++;
+			}
+			System.out.println (num);
+		}
+		else 
+		{
+			System.out.println("Zero in not acceptable");
+		}
 	}
 }
+		
--- Perfect.java
+++ Perfect.java
@@ -1,8 +1,34 @@
-/**
- *  Gets a command-line argument (int), and chekcs if the given number is perfect.
- */
 public class Perfect {
-	public static void main (String[] args) {
-		//// Put your code here
+	public static void main(String[] args) {
+		int num = Integer.parseInt ( args[0] );
+		if (num == 0 || num == 1 || num == 2)
+		{
+			System.out.println(num + " is not a perfect number");
+		}
+		else
+		{
+			int sum = 1;
+			int div = 2;
+			String s = num + " is a perfect number since " + num + " = 1";
+			while (div<num && sum <=num)
+			{
+				if ((num%div)==0)
+				{
+					s = s + " + " + div;
+					sum = sum + div;
+				}
+				div++;
+			}
+			if (sum == num)
+			{
+				System.out.println (s);
+			}
+			else
+			{
+				System.out.println(num + " is not a perfect number");
+			}
+		}
+	
 	}
 }
+		
